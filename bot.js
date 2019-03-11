e571217885aa6bba516d89e62dfb1db7297d80ea
Luiz Henrique <luizhenrique98@gmail.com>
2019-03-11 11:34:04 -0300

    Fix

--- bot.js
+++ bot.js
@@ -16,40 +16,52 @@ class MyBot {
         {
             var id = 1
     
-            for(i = 0; i < data.length; i ++)
+            for(var i = 0; i < data.length; i ++)
             {
                 if(data[i].name.includes(name))
                     id = data[i].id
             }
     
+            console.log(id)
             return id
         }
 
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        var data
+        // var data
+
+        // var url = "https://261ad154.ngrok.io/api/politician/"
+        //     const api = axios.create({
+        //         baseURL: url,
+        //         });
+        //         api.defaults.headers.common['Content-Type'] = "application/json";
+        //         api.get().then(function (response) {
+        //             // handle success
+        //             console.log(response.data)
+        //             data = response
+        //         })
+        //         .catch(function (error) {
+        //             // handle error
+        //             console.log(error);
+        //         })
+        //         .then(function () {
+        //             // always executed
+        //     });
+        
+        if (turnContext.activity.type === ActivityTypes.Message) {
+            var text = turnContext.activity.text
+            var data
+            var id = 1
 
         var url = "https://261ad154.ngrok.io/api/politician/"
             const api = axios.create({
                 baseURL: url,
                 });
                 api.defaults.headers.common['Content-Type'] = "application/json";
-                api.get().then(function (response) {
-                    // handle success
-                    console.log(response)
-                    data = response
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-                .then(function () {
-                    // always executed
-            });
-
-        if (turnContext.activity.type === ActivityTypes.Message) {
-            var text = turnContext.activity.text    
-            var id = findByName(data.data, text)
-            await turnContext.sendActivity(`You said '${ id }'`);
+                var response = await  api.get()
+                console.log(response.data)
+                data = response
+                id = findByName(data.data, text)
+                await turnContext.sendActivity(`You said '${ id }'`);
         } else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }

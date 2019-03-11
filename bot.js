9c16a43cf47dd8ac65d96ae75974a21e2a97fcd2
Luiz Henrique <luizhenrique98@gmail.com>
2019-03-11 11:18:23 -0300

    Add find

--- bot.js
+++ bot.js
@@ -11,26 +11,45 @@ class MyBot {
      * @param {TurnContext} on turn context object.
      */
     async onTurn(turnContext) {
+
+        function findByName(data, name)
+        {
+            var id = 1
+    
+            for(i = 0; i < data.length; i ++)
+            {
+                if(data[i].name.includes(name))
+                    id = data[i].id
+            }
+    
+            return id
+        }
+
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        var data
 
         var url = "https://261ad154.ngrok.io/api/politician/"
-        const api = axios.create({
-            baseURL: url,
-          });
-          api.defaults.headers.common['Content-Type'] = "application/json";
-          api.get().then(function (response) {
-            // handle success
-            console.log(response)
-          })
-          .catch(function (error) {
-            // handle error
-            console.log(error);
-          })
-          .then(function () {
-            // always executed
-        });
+            const api = axios.create({
+                baseURL: url,
+                });
+                api.defaults.headers.common['Content-Type'] = "application/json";
+                api.get().then(function (response) {
+                    // handle success
+                    console.log(response)
+                    data = response
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+            });
+
         if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
+            var text = turnContext.activity.text    
+            var id = findByName(data.data, text)
+            await turnContext.sendActivity(`You said '${ id }'`);
         } else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }
